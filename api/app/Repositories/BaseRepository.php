b8d40af79c7d488b7e496a270a28fdcf54b5ba90
cmVitor <vitor.martins.castanheira@gmail.com>
2025-12-01 20:51:09 -0300

    feat: create repositories to access the db tables

---
+++ api/app/Repositories/BaseRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Interfaces\BaseRepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository implements BaseRepositoryInterface
+{
+    protected Model $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function find(int $id)
+    {
+        return $this->model->findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $record = $this->find($id);
+        $record->update($data);
+        return $record;
+    }
+
+    public function delete(int $id)
+    {
+        $record = $this->find($id);
+        return $record->delete();
+    }
+
+    public function paginate(int $perPage = 10, array $relations = [])
+    {
+        return $this->model->with($relations)->paginate($perPage);
+    }
+}
